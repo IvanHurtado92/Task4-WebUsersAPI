b2b7b30ce291b0e75c2b0e52b8ce13c52b2285aa
Ivan Hurtado <ivan.hurtado92@outlook.com>
2025-05-26 22:43:37 -0600

    Commentary in User

--- Task4-WebUsersAPI/Models/User.cs
+++ Task4-WebUsersAPI/Models/User.cs
@@ -6,10 +6,10 @@ namespace Task4_WebUsersAPI.Models
     {
         [Key]
         public int IdUser { get; set; } // Se detecta como Primary Key automaticamente si solo se llama Id
-        public string Name { get; set; } = null!;
+        public string Name { get; set; } = null!; // Not Null
         public string Email { get; set; } = null!;
         public string Password { get; set; } = null!;
-        public bool? Blocked { get; set; }
+        public bool? Blocked { get; set; } // ? indica que puede ser null
         public DateTime? Last_Connection {  get; set; }
     }
 }
